c4f365d26a57a865c9896898511e911fd69403d4
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-17 09:56:46 +0530

    Added a step to create manifests before bringing up cluster

--- deploy-as-code/cluster_up.sh
+++ deploy-as-code/cluster_up.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Create namespaces
+kubectl create -f definitions/cluster/egov-namespaces.yml
+
 # Create services
+kubectl create -f definitions/cluster/web/services
 kubectl create -f definitions/cluster/backbone/services
 kubectl create -f definitions/cluster/common/services
 kubectl create -f definitions/cluster/pgr/services
@@ -10,6 +14,7 @@ kubectl create -f definitions/cluster/common/volumes
 kubectl create -f definitions/cluster/common/volume_claims
 
 # Create deployments
+kubectl create -f definitions/cluster/web/deployments
 kubectl create -f definitions/cluster/backbone/deployments
 kubectl create -f definitions/cluster/common/deployments
 kubectl create -f definitions/cluster/pgr/deployments

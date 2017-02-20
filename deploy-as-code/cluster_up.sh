6793acbe982124edc5b7c27ee857d902bbdc4f8e
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-20 15:07:31 +0530

    Renamed cluster resources to match their app name in github

--- deploy-as-code/cluster_up.sh
+++ deploy-as-code/cluster_up.sh
@@ -3,18 +3,8 @@
 # Create namespaces
 kubectl create -f definitions/cluster/egov-namespaces.yml
 
-# Create services
-kubectl create -f definitions/cluster/web/services
-kubectl create -f definitions/cluster/backbone/services
-kubectl create -f definitions/cluster/common/services
-kubectl create -f definitions/cluster/pgr/services
-
-# Create persistent-volumes and persisten-volume-claims
-kubectl create -f definitions/cluster/common/volumes
-kubectl create -f definitions/cluster/common/volume_claims
-
-# Create deployments
-kubectl create -f definitions/cluster/web/deployments
-kubectl create -f definitions/cluster/backbone/deployments
-kubectl create -f definitions/cluster/common/deployments
-kubectl create -f definitions/cluster/pgr/deployments
+# Create apps in all namespaces
+kubectl create -f definitions/cluster/app/backbone
+kubectl create -f definitions/cluster/app/web
+kubectl create -f definitions/cluster/app/common
+kubectl create -f definitions/cluster/app/pgr

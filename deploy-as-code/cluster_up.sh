9166e8133866b6eead1b95b30687e4a8004de686
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-21 22:56:59 +0530

    Renamed common ns to core

--- deploy-as-code/cluster_up.sh
+++ deploy-as-code/cluster_up.sh
@@ -5,7 +5,6 @@ kubectl apply -f definitions/cluster/egov-namespaces.yml
 
 # Create apps in all namespaces
 kubectl apply -f definitions/cluster/app/backbone
-kubectl apply -f definitions/cluster/app/web
-kubectl apply -f definitions/cluster/app/common
+kubectl apply -f definitions/cluster/app/core
 kubectl apply -f definitions/cluster/app/pgr
 kubectl apply -f definitions/cluster/app/lams

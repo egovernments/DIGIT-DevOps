ebc04dd2dc45d0fbe2e8e19725dcef77f4649215
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-25 15:56:30 +0530

    Modified elastic search cluster

--- deploy-as-code/cluster_up.sh
+++ deploy-as-code/cluster_up.sh
@@ -3,8 +3,15 @@
 # Create namespaces
 kubectl apply -f definitions/cluster/egov-namespaces.yml
 
+# Create add-ons
+kubectl apply -f definitions/cluster/addons/fluentd-elasticsearch-logging
+
+# Create persistent volumes
+kubectl apply -f definitions/cluster/volume
+
 # Create apps in all namespaces
 kubectl apply -f definitions/cluster/app/backbone
 kubectl apply -f definitions/cluster/app/core
 kubectl apply -f definitions/cluster/app/pgr
 kubectl apply -f definitions/cluster/app/lams
+kubectl apply -f definitions/cluster/app/elasticsearch

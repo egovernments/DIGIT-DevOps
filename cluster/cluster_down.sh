9a938fb31e867b93cb3d169f8a9b88af5b858886
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-03-02 20:29:32 +0530

    Refactored all manifests to use configMaps and secrets

--- cluster/cluster_down.sh
+++ cluster/cluster_down.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for n in ( core, pgr, lams, logging, es-cluster, default );do
-kubectl delete svc,deployments,pvc,pv,configMaps,statefulset,ds,secret,rc --all --namespace=$n
+for n in egov;
+do
+    kubectl delete svc,deployments,pvc,pv,configMaps,statefulset,ds,secret,rc --all --namespace=$n
 done

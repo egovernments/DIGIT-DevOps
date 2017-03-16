3963c113027fc0b5438daa016fd79f75b06a39fd
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-03-16 18:41:53 +0530

    Made fluentd talk to es-cluster. Removed redundant kibana and es for logging

---
+++ dockerfiles/fluentd/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "$ES_PORT" ]; then
+    export ES_PORT=9200
+fi
+
+if [ -z "$ES_HOST" ]; then
+    export ES_HOST=elasticseatch-logging
+fi
+
+
+sed -i 's/host ES_HOST/host '"$ES_HOST"'/' /etc/td-agent/td-agent.conf
+sed -i 's/port ES_PORT/port '"$ES_PORT"'/' /etc/td-agent/td-agent.conf
+
+/usr/sbin/td-agent 2>&1 >> /var/log/fluentd.log

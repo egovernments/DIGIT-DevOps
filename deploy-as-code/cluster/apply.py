c17a91f286212757216b769029c9a5affdd39f99
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-04-17 16:10:05 +0530

    Updating jenkins deployment pipeline

--- deploy-as-code/cluster/apply.py
+++ deploy-as-code/cluster/apply.py
@@ -46,7 +46,7 @@ def find_config_vars(args):
     if not os.path.isfile(env_file_path):
         raise Exception("No config found for env - {}".format(args.env))
     conf = yaml.load(open(env_file_path, "r"))
-    if not conf[args.microservice]:
+    if not conf.has_key(args.microservice):
         conf[args.microservice] = {}
     if args.image:
         conf[args.microservice]['image'] = args.image

359d4bae9d30d9acaa4a24a217e3ca795cc0402e
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-04-17 10:20:28 +0530

    Added jenkins pipeline for deployment

---
+++ jenkins/deployer.groovy
@@ -0,0 +1,46 @@
+kubectl_image = "egovio/kubectl:0.0.1"
+
+def takeSnapshot(group, env){
+    stage("Snapshot ${env} env"){
+        def cmd = "python jenkins/scripts/snapshot.py ${group}"
+        run(env, cmd)
+    }
+}
+
+def deploy(env){
+    stage("Deploy to ${env} env"){
+        def cmd = "python jenkins/scripts/deploy.py"
+        run(env, cmd)
+    }
+}
+
+def deployStandAlone(env, service, tag){
+    stage("Deploy to ${service}:${tag} to ${env} env"){
+        def cmd = "python cluster/apply.py  -e dev -m ${service} -i egovio/${service}:${tag} -dmi egovio/${service}-db:${tag} -d"
+        run(env, cmd)
+    }
+}
+
+def run(env, cmd){
+    docker.image("${kubectl_image}").inside {
+        set_kube_credentials(env)
+        withCredentials([string(credentialsId: "${env}-kube-url", variable: "KUBE_SERVER_URL")]){
+            sh "kubectl config set-cluster env --server ${KUBE_SERVER_URL}"
+        }
+        sh cmd;
+    }
+}
+
+def set_kube_credentials(env){
+    withCredentials([file(credentialsId: "${env}-kube-ca", variable: "CA")]){
+        sh "cp ${CA} /kube/ca.pem"
+    }
+    withCredentials([file(credentialsId: "${env}-kube-cert", variable: "CERT")]){
+        sh "cp ${CERT} /kube/admin.pem"
+    }
+    withCredentials([file(credentialsId: "${env}-kube-key", variable: "CERT_KEY")]){
+        sh "cp ${CERT_KEY} /kube/admin-key.pem"
+    }
+}
+
+return this;

fe9c05390360df30f1b6ea5428b106a6068995af
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-05-22 11:14:22 +0530

    Upgraded fluentd to concat all exceptions

--- deploy-as-code/dockerfiles/fluentd/start.sh
+++ deploy-as-code/dockerfiles/fluentd/start.sh
@@ -12,4 +12,4 @@ fi
 sed -i 's/host ES_HOST/host '"$ES_HOST"'/' /etc/td-agent/td-agent.conf
 sed -i 's/port ES_PORT/port '"$ES_PORT"'/' /etc/td-agent/td-agent.conf
 
-/usr/sbin/td-agent 2>&1 >> /var/log/fluentd.log
+/usr/sbin/td-agent 2>&1 | tee -a /var/log/fluentd.log

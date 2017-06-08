e2482ff76c96edadf783215840255b93b2766b6c
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-06-08 14:37:55 +0530

    Made index name driven by target env for fluentd logging and es curator

--- dockerfiles/fluentd/start.sh
+++ dockerfiles/fluentd/start.sh
@@ -8,8 +8,13 @@ if [ -z "$ES_HOST" ]; then
     export ES_HOST=elasticsearch-logging
 fi
 
+if [ -z "$TARGET_ENV" ]; then
+    export TARGET_ENV=default
+fi
+
 
 sed -i 's/host ES_HOST/host '"$ES_HOST"'/' /etc/td-agent/td-agent.conf
 sed -i 's/port ES_PORT/port '"$ES_PORT"'/' /etc/td-agent/td-agent.conf
+sed -i 's/%TARGET_ENV%/'"$TARGET_ENV"'/' /etc/td-agent/td-agent.conf
 
 /usr/sbin/td-agent 2>&1 | tee -a /var/log/fluentd.log

237ece3874e1afe6e5610c18d17c0d13903c2c69
Senthil <senthil.kalimuthu@tarento.com>
2017-08-20 19:46:35 +0530

    Increasing heap and memory for pgr-master and egov-user on mahaulb

--- deploy-as-code/dockerfiles/cloudwatch_push_mem_metrics/entrypoint.sh
+++ deploy-as-code/dockerfiles/cloudwatch_push_mem_metrics/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true; do
-  ./mon-put-instance-data.pl --mem-util --mem-used --mem-avail \
-  --disk-space-avail --disk-space-used --disk-space-util --disk-path=/rootfs \
-  --swap-util --swap-used \
+  ./mon-put-instance-data.pl --mem-util  --mem-avail \
+  --disk-space-util --disk-path=/rootfs \
+  --swap-util --aggregated\
   --aws-access-key-id $AWS_ACCESS_KEY_ID --aws-secret-key $AWS_SECRET_ACCESS_KEY
   sleep 60
 done

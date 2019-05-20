e37b365776e901f38fd65a45b64d793bdd327748
Gajendran <40357140+gajendran-egov@users.noreply.github.com>
2019-05-20 12:21:57 +0530

    Adding ukd to the CI env

--- jenkins/deployer.groovy
+++ jenkins/deployer.groovy
@@ -44,7 +44,7 @@ def set_kube_credentials(env){
         sh "cp ${CERT_KEY} /kube/admin-key.pem"
     }
 
-    if (env == "apUat" || env == "apProd" || env == "playground" || env == "qa" ) {
+    if (env == "apUat" || env == "apProd" || env == "playground" || env == "qa" || env == "ukd-uat") {
         withCredentials([string(credentialsId: "${env}-kube-token", variable: "TOKEN")]){
             sh "kubectl config set-credentials env --token ${TOKEN}"
         }

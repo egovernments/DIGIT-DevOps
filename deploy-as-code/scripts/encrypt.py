32172e76980d8c253dd15473bc6dc4e775be1b3d
Nithin <nithin_1992@msn.com>
2019-05-24 11:20:55 +0530

    UI runtime configuration
     - UI runtime configuration
     - Modify encrypt logic to auto adjust padding

--- deploy-as-code/scripts/encrypt.py
+++ deploy-as-code/scripts/encrypt.py
@@ -2,6 +2,7 @@
 import base64
 import os
 import sys
+import math
 
 from Crypto.Cipher import AES
 
@@ -9,10 +10,13 @@ from Crypto.Cipher import AES
 def main():
     # Because kubernetes expects the values in secrets to be base64 encoded
     b64_encodedtext = base64.b64encode(sys.argv[1])
+    padding_length = len(b64_encodedtext)
+    if padding_length % 16 != 0 :
+        padding_length = padding_length + (16 - padding_length % 16)
 
     key = os.environ["EGOV_SECRET_PASSCODE"]
     encryptor = AES.new(key, AES.MODE_ECB)
-    print base64.b64encode(encryptor.encrypt(b64_encodedtext.rjust(80)))
+    print base64.b64encode(encryptor.encrypt(b64_encodedtext.rjust(padding_length)))
 
 
 if __name__ == "__main__":

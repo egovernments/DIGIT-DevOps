abbdbf163113502c5d2a2b0d2656059c1e37d090
Nithin <nithin_1992@msn.com>
2019-05-28 17:16:43 +0530

    Update SSL certs - bihar uat

--- deploy-as-code/scripts/encrypt.py
+++ deploy-as-code/scripts/encrypt.py
@@ -9,14 +9,15 @@ from Crypto.Cipher import AES
 
 def main():
     # Because kubernetes expects the values in secrets to be base64 encoded
-    b64_encodedtext = base64.b64encode(sys.argv[1])
-    padding_length = len(b64_encodedtext)
+    text_to_encrypt = sys.argv[1]
+    text_to_encrypt = base64.b64encode(text_to_encrypt)
+    padding_length = len(text_to_encrypt)
     if padding_length % 16 != 0 :
         padding_length = padding_length + (16 - padding_length % 16)
 
     key = os.environ["EGOV_SECRET_PASSCODE"]
     encryptor = AES.new(key, AES.MODE_ECB)
-    print base64.b64encode(encryptor.encrypt(b64_encodedtext.rjust(padding_length)))
+    print base64.b64encode(encryptor.encrypt(text_to_encrypt.rjust(padding_length)))
 
 
 if __name__ == "__main__":

929d232044f0c2389e101e7c160543b5d322e9de
Nithin <36623418+nithindv@users.noreply.github.com>
2019-06-06 22:03:52 +0530

    Modify kube config generation

--- deploy-as-code/jenkins/deployer.groovy
+++ deploy-as-code/jenkins/deployer.groovy
@@ -23,7 +23,6 @@ def deployStandAlone(env, service, image, tag){
 
 def run(env, cmd){
     docker.image("${deployer_image}").inside {
-        if (env == "pbuatv2") {
             set_kube_config(env)
             
             withCredentials([
@@ -34,46 +33,7 @@ def run(env, cmd){
             ]){
                 sh cmd;
             }
-            
-        } else{
-            set_kube_credentials(env)
-            withCredentials([
-                string(credentialsId: "${env}-kube-url", variable: "KUBE_SERVER_URL"),
-                string(credentialsId: "egov_secret_passcode", variable: "EGOV_SECRET_PASSCODE")
-            ]){
-                sh "kubectl config set-cluster env --server ${KUBE_SERVER_URL}"
-                sh cmd;
-            }
-        }
     }
-}
-
-def set_kube_credentials(env){
-    withCredentials([
-        file(credentialsId: "${env}-kube-ca", variable: "CA"),
-        file(credentialsId: "${env}-kube-cert", variable: "CERT"),
-        file(credentialsId: "${env}-kube-key", variable: "CERT_KEY")
-    ]){
-        sh "cp ${CA} /kube/ca.pem"
-        sh "cp ${CERT} /kube/admin.pem"
-        sh "cp ${CERT_KEY} /kube/admin-key.pem"
-    }
-
-    if (env == "apUat" || env == "apProd" || env == "playground" || env == "qa" || env == "ukd-uat") {
-        withCredentials([string(credentialsId: "${env}-kube-token", variable: "TOKEN")]){
-            sh "kubectl config set-credentials env --token ${TOKEN}"
-        }
-    }
-
-    if (env == "pbuat" || env == "pbprod" || env=="dev" || env =="demoenv2") {
-        withCredentials([string(credentialsId: "${env}-kube-username", variable: "AUTHUSER")]){
-            sh "kubectl config set-credentials env --username=${AUTHUSER}"
-        }
-        withCredentials([string(credentialsId: "${env}-kube-password", variable: "AUTHPASSWORD")]){
-            sh "kubectl config set-credentials env --password=${AUTHPASSWORD}"
-        }
-    }
-
 }
 
 def set_kube_config(env){

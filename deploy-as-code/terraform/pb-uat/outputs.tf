0a23ce78ad1f4f55521f8f99eeef3a050b877ee5
Nithin <nithin_1992@msn.com>
2019-06-13 22:31:48 +0530

    Update configs for cluster migration
    - Update configs, volumes etc
    - Add terraform files for pbuat
    - Add terraform aws storage module

--- deploy-as-code/terraform/pb-uat/outputs.tf
+++ deploy-as-code/terraform/pb-uat/outputs.tf
@@ -29,3 +29,27 @@ output "kubeconfig" {
 output "eks_cluster" {
   value = module.eks-cluster.eks_cluster
 }
+
+output "es_master_volume_ids" {
+  value = "${module.es-master.volume_ids}"
+}
+
+output "es_data_volume_ids" {
+  value = "${module.es-data-v1.volume_ids}"
+}
+
+output "zookeeper_volume_ids" {
+  value = "${module.zookeeper.volume_ids}"
+}
+
+output "kafka_vol_by_snapshots" {
+  value = "${aws_ebs_volume.vol_by_snapshots.*.id}"
+}
+
+output "kafka_vol_1c" {
+  value = "${aws_ebs_volume.kafka_1c_vol}"
+}
+
+output "kafka_infra_vol_ids" {
+  value = "${module.kafka-infra.volume_ids}"
+}

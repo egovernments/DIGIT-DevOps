f5699dff3034cd5ba6efffc6fa198bfc0ace3047
Nithin <36623418+nithindv@users.noreply.github.com>
2019-08-24 02:12:35 +0530

    Update buildPipeline.groovy

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -81,7 +81,7 @@ spec:
                 echo \"Attempting to build image,  ${image}\"
                 /kaniko/executor -f `pwd`/${buildConfig.getDockerFile()} -c `pwd`/${buildConfig.getContext()} \
                 --build-arg WORK_DIR=${buildConfig.getWorkDir()} \
-                --cache=true --cache-repo=${REPO_NAME}/kaniko-cache --single-snapshot \
+                --cache=true --cache-dir=/cache --single-snapshot \
                 --destination=${image}
                         """)
 

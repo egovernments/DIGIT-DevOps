5ffdb41e0d03f3d2b28f2f7ec6c59a4f22dc0db4
Nithin <36623418+nithindv@users.noreply.github.com>
2019-08-28 00:42:44 +0530

    Update buildPipeline.groovy

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -67,6 +67,8 @@ spec:
                  'git log --oneline -- ${BUILD_PATH} | awk \'NR==1{print $1}\'',
                   returnStdout: true).trim()
                             scmVars['ACTUAL_COMMIT'] = scmVars['ACTUAL_COMMIT'].replaceFirst("origin/", "")
+                            println test.indexOf("origin/")
+                            println scmVars['ACTUAL_COMMIT']
                         }
                     }
                 }

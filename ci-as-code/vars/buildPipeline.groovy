27d860a5fad7aac06d7c0dfadd9d2274984c484c
Nithin <36623418+nithindv@users.noreply.github.com>
2019-09-18 16:07:59 +0530

    Update buildPipeline.groovy

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -14,7 +14,7 @@ metadata:
 spec:
   containers:
   - name: kaniko
-    image: gcr.io/kaniko-project/executor:debug-v0.11.0
+    image: gcr.io/kaniko-project/executor:debug-v0.8.0
     imagePullPolicy: IfNotPresent
     command:
     - /busybox/cat

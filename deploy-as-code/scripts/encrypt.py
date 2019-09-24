90c1c80ce3079b38f0d2509a2949b002aa34bf29
Nithin DV <nithin_1992@msn.com>
2019-09-24 18:36:57 +0530

    Encryption of file

--- deploy-as-code/scripts/encrypt.py
+++ deploy-as-code/scripts/encrypt.py
@@ -3,13 +3,29 @@ import base64
 import os
 import sys
 import math
+import argparse
 
 from Crypto.Cipher import AES
 
 
+parser = argparse.ArgumentParser()
+
+def parse_args():
+    parser.add_argument("-f", "--file", help="File to be encrypted")
+    args, unknown = parser.parse_known_args()
+
+    return args
+
 def main():
+    args = parse_args()
+    if args.file :
+        f=open(args.file, "r")
+        text_to_encrypt = f.read()
+    else :
+        text_to_encrypt = sys.argv[1]
+
     # Because kubernetes expects the values in secrets to be base64 encoded
-    text_to_encrypt = sys.argv[1]
+    # text_to_encrypt = sys.argv[1]
     text_to_encrypt = base64.b64encode(text_to_encrypt)
     padding_length = len(text_to_encrypt)
     if padding_length % 16 != 0 :

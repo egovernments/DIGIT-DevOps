5ae1a965340fb7aa7b29f8ad07fe374811e39470
Nithin <36623418+nithindv@users.noreply.github.com>
2019-12-18 09:29:27 +0530

    Escape $

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -100,6 +100,7 @@ spec:
                                     echo \"Attempting to build image,  ${image}\"
                                     /kaniko/executor -f `pwd`/${buildConfig.getDockerFile()} -c `pwd`/${buildConfig.getContext()} \
                                     --build-arg WORK_DIR=${workDir} \
+                                    --build-arg GIT_ACCESS_TOKEN=\\$GIT_ACCESS_TOKEN \
                                     --cache=true --cache-dir=/cache \
                                     --single-snapshot=true \
                                     --snapshotMode=time \

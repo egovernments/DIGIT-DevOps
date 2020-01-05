9b4b719178cdca6ef0b62edc75b89366b78ef4a2
sheikiq-McSkies-egov <58679247+sheikiq-McSkies-egov@users.noreply.github.com>
2020-01-05 16:22:58 +0530

    Creating UKD_DEV and BHR_DEV cluster (#169)

---
+++ deploy-as-code/terraform/ukd-dev/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+    default = "ukd-dev"
+}
+variable "resource_group" {
+    default = "ukd-dev"
+}
+
+variable "location" {
+    default = "SouthIndia"
+}
+
+variable "db_password" {
+}
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}

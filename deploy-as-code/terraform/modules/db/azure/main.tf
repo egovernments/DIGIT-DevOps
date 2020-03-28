1a385f2ad17b5c99c70687a9ae1e298f7d403956
Nithin <36623418+nithindv@users.noreply.github.com>
2020-03-29 01:40:30 +0530

    Add epass prod, add terraform files

--- deploy-as-code/terraform/modules/db/azure/main.tf
+++ deploy-as-code/terraform/modules/db/azure/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_postgresql_server" "postgresql_server" {
   location             = "${var.location}"
   resource_group_name  = "${var.resource_group}"
 
-  sku_name = "B_Gen5_2"
+  sku_name = "${var.sku_tier}"
 
   storage_profile {
     storage_mb            = "${var.storage_mb}"

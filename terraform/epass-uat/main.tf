c7c66319160d4ff98eaa8389beef7b6a4e741244
Nithin <nithin_1992@msn.com>
2020-04-20 17:32:49 +0530

    Add epass-uat & epass-prod AWS clusters

--- terraform/epass-uat/main.tf
+++ terraform/epass-uat/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "covidpass-terraform-state"
+    bucket = "epass-uat-terraform-state"
     key = "terraform"
     region = "ap-south-1"
   }
@@ -10,7 +10,7 @@ module "network" {
   source             = "../modules/kubernetes/aws/network"
   vpc_cidr_block     = "${var.vpc_cidr_block}"
   cluster_name       = "${var.cluster_name}"
-  availability_zones = "${var.availability_zones}"
+  availability_zones = "${var.network_availability_zones}"
 }
 
 module "db" {
@@ -23,35 +23,70 @@ module "db" {
   storage_type                  = "gp2"
   storage_gb                    = "100"
   backup_retention_days         = "7"
-  administrator_login           = "coviduat"
+  administrator_login           = "epassuat"
   administrator_login_password  = "${var.db_password}"
   db_name                       = "${var.cluster_name}-db"
   environment                   = "${var.cluster_name}"
 }
 
-module "eks-cluster" {
-  source                        = "../modules/kubernetes/aws/eks-cluster"
-  cluster_name                  = "${var.cluster_name}"
-  kubernetes_version            = "${var.kubernetes_version}"
-  vpc_id                        = "${module.network.vpc_id}"
-  subnets                       = "${concat(module.network.private_subnets, module.network.public_subnets)}"
-  master_nodes_security_grp_ids = ["${module.network.master_nodes_sg_id}"]
+data "aws_eks_cluster" "cluster" {
+  name = "${module.eks.cluster_id}"
 }
 
-module "worker-nodes" {
-  source                        = "../modules/kubernetes/aws/workers"
-  cluster_name                  = "${var.cluster_name}"
-  instance_type                 = "${var.instance_type}"
-  number_of_worker_nodes        = "${var.number_of_worker_nodes}"
-  ssh_key_name                  = "${var.ssh_key_name}"
-  vpc_id                        = "${module.network.vpc_id}"
-  eks_cluster                   = "${module.eks-cluster.eks_cluster}"
-  subnets                       = "${module.network.private_subnets}"
-  worker_nodes_security_grp_ids = ["${module.network.worker_nodes_sg_id}"]
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "${var.cluster_name}"
+  cluster_version = "${var.kubernetes_version}"
+  subnets         = "${concat(slice(module.network.private_subnets, 0, length(var.availability_zones)), slice(module.network.public_subnets, 0, length(var.availability_zones)))}"
+
+  tags = "${
+    map(
+      "kubernetes.io/cluster/${var.cluster_name}", "owned",
+      "KubernetesCluster", "${var.cluster_name}"
+    )
+  }"
+
+  vpc_id = "${module.network.vpc_id}"
+
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = 50
+    subnets   = "${slice(module.network.private_subnets, 0, length(var.availability_zones))}"
+    # key_name  = "${var.ssh_key_name}"
+  }
+
+  node_groups = {
+    example = {
+      name    = "${var.cluster_name}-nodegroup"
+      desired_capacity = "${var.number_of_worker_nodes}"
+
+      instance_type = "${var.instance_type}"
+      k8s_labels = {
+        Environment = "${var.cluster_name}"
+      }
+      
+    }
+  }
+  
+  map_users    = var.map_users
 }
 
 module "es-master" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "es-master"
   availability_zones = "${var.availability_zones}"
@@ -60,7 +95,9 @@ module "es-master" {
   
 }
 module "es-data-v1" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "es-data-v1"
   availability_zones = "${var.availability_zones}"
@@ -70,7 +107,9 @@ module "es-data-v1" {
 }
 
 module "es-master-infra" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "es-master-infra"
   availability_zones = "${var.availability_zones}"
@@ -79,7 +118,9 @@ module "es-master-infra" {
   
 }
 module "es-data-infra-v1" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "es-data-infra-v1"
   availability_zones = "${var.availability_zones}"
@@ -89,7 +130,9 @@ module "es-data-infra-v1" {
 }
 
 module "zookeeper" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "zookeeper"
   availability_zones = "${var.availability_zones}"
@@ -99,7 +142,9 @@ module "zookeeper" {
 }
 
 module "kafka" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "kafka"
   availability_zones = "${var.availability_zones}"
@@ -109,10 +154,13 @@ module "kafka" {
 }
 
 module "kafka-infra" {
+
   source = "../modules/storage/aws"
+  storage_count = 3
   environment = "${var.cluster_name}"
   disk_prefix = "kafka-infra"
   availability_zones = "${var.availability_zones}"
   storage_sku = "st1"
-  disk_size_gb = "500"  
+  disk_size_gb = "500"
+  
 }

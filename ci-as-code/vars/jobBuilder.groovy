c9296b36e257d77adfae3c9f6c3fbaff11038967
Nithin <36623418+nithindv@users.noreply.github.com>
2020-04-29 16:11:37 +0530

    Allow for branches and tags

--- ci-as-code/vars/jobBuilder.groovy
+++ ci-as-code/vars/jobBuilder.groovy
@@ -89,7 +89,7 @@ spec:
                 parameters {  
                   gitParameterDefinition {
                         name('BRANCH')
-                        type('BRANCH_TAG')
+                        type('PT_BRANCH_TAG')
                         description('') 
                         branch('')      
                         useRepository('')                     

c029ab4ac92d341f061af7ff58cb751d033003eb
Tarun Lalwani <tarunlalwani@users.noreply.github.com>
2020-06-30 08:08:29 +0530

    Use VERSION file for build (#708)

--- deploy-as-code/dockerfiles/builder/get_application_version.sh
+++ deploy-as-code/dockerfiles/builder/get_application_version.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-if [ -f "$1pom.xml" ]; then
-    cat pom.xml | oq -i xml -r '.project.version' | cut -d'-' -f1
-elif [ -f "$1package.json" ]; then
-    cat package.json | jq -r '.version'
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    FOLDER=
+else
+    FOLDER=$1/
+fi
+
+if [ -f "${FOLDER}pom.xml" ]; then
+    cat ${FOLDER}pom.xml | oq -i xml -r '.project.version' | cut -d'-' -f1
+elif [ -f "${FOLDER}package.json" ]; then
+    cat ${FOLDER}package.json | jq -r '.version'
+elif [ -f "${FOLDER}VERSION" ]; then
+    cat ${FOLDER}VERSION
 else
     echo "NA"
 fi

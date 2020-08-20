002f11db6494d5ff777db04b69a853b6f880057e
gajendran.c <gajendran.c@egovernments.org>
2020-08-20 10:12:32 +0530

    Build Number signature change

--- ci-as-code/vars/buildPipeline.groovy
+++ ci-as-code/vars/buildPipeline.groovy
@@ -118,9 +118,9 @@ spec:
                                 String workDir = buildConfig.getWorkDir().replaceFirst(getCommonBasePath(buildConfig.getWorkDir(), buildConfig.getDockerFile()), "./")
                                 String image = null;
                                 if(scmVars.BRANCH.equalsIgnoreCase("master")) {
-                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:${env.BUILD_NUMBER}-v${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}";
+                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:v${scmVars.VERSION}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}";
                                 } else {
-                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:${env.BUILD_NUMBER}-${scmVars.BRANCH}-${scmVars.ACTUAL_COMMIT}";
+                                  image = "${REPO_NAME}/${buildConfig.getImageName()}:${scmVars.BRANCH}-${scmVars.ACTUAL_COMMIT}-${env.BUILD_NUMBER}";
                                 } 
                                 serviceCategory = buildConfig.getServiceCategoryName();  // Dashboard
                                 buildNum = "${scmVars.VERSION}"; // Dashboard

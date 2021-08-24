b186a272d00534f3e7b32df7ec19f29cdddd59b8
nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
2021-08-24 18:37:17 +0530

    oci tf (#311)
    * added oci terraform script
    
    * added variables
    
    * removed tag
    
    * add outputs.tf
    
    Co-authored-by: basilvg-eGov <basil.varghese@egovernments.org>

---
+++ infra-as-code/terraform/modules/kubernetes/oci/network/variables.tf
@@ -0,0 +1,5 @@
+variable "vcn_cidr" {}
+variable "tenancy_id" {}    
+variable "ClusterName" {}
+variable "dns-label" {}
+
---
+++ infra-as-code/terraform/modules/storage/oci/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_count" {}
+variable "compartment_id" {}    
+variable "vol_name" {}
+variable "block_storage_sizes_in_gbs" {}
+variable "ad" {}

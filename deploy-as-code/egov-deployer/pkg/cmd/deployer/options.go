e3461a61eb3881884265bff7cb5394233b94b72d
Gajendran <40357140+gajendran-egov@users.noreply.github.com>
2022-01-10 18:16:02 +0530

    Merge Deployer Changes from Digit 3.0 (#584)
    * Rebase with master
    
    * new-deployer script
    
    * Remove redundant chart folders after rebase

--- deploy-as-code/egov-deployer/pkg/cmd/deployer/options.go
+++ deploy-as-code/egov-deployer/pkg/cmd/deployer/options.go
@@ -1,38 +1,11 @@
 package deployer
 
 type Options struct {
+	DesiredProduct string
+	ProductVersion string
 	HelmDir        string
 	Images         string
 	Environment    string
 	ClusterConfigs bool
 	Print          bool
 }
-
-// func (o *Options) SetImages(images string) {
-// 	o.images = images
-// }
-
-// func (o Options) Images() string {
-// 	return o.images
-
-// }
-
-// func (o Options) Environment() string {
-// 	return o.environment
-
-// }
-
-// func (o Options) ClusterConfigs() bool {
-// 	return o.clusterConfigs
-
-// }
-
-// func (o Options) DryRun() bool {
-// 	return o.dryRun
-
-// }
-
-// func (o Options) Print() bool {
-// 	return o.print
-
-// }

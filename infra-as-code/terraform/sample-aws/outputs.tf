26c189a825ce82de4b5ce0779384e6ad02a0ba8f
Nikesh <nikeshumredkar@nikesh.local>
2022-05-18 16:10:50 +0530

    enhanced sample aws tf

--- infra-as-code/terraform/sample-aws/outputs.tf
+++ infra-as-code/terraform/sample-aws/outputs.tf
@@ -37,3 +37,24 @@ output "zookeeper_volume_ids" {
 output "kafka_vol_ids" {
   value = "${module.kafka.volume_ids}"
 }
+
+output "db_instance_endpoint" {
+  value = module.db.db_instance_endpoint
+}
+
+
+output "db_instance_name" {
+  description = "The database name"
+  value       = module.db.db_instance_name
+}
+
+output "db_instance_username" {
+  description = "The master username for the database"
+  value       = module.db.db_instance_username
+  sensitive   = true
+}
+
+output "db_instance_port" {
+  description = "The database port"
+  value       = module.db.db_instance_port
+}

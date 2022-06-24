864cf5e750fb4c875ff651b9f7b5665987308a70
nikesh-eGov <48427967+nikesh-eGov@users.noreply.github.com>
2022-06-24 18:15:53 +0530

    Update main.tf

--- infra-as-code/terraform/egov-cicd/main.tf
+++ infra-as-code/terraform/egov-cicd/main.tf
@@ -21,7 +21,14 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = "${module.eks.cluster_id}"
 }
-
+  
+provider "kubernetes" {
+  host                   = "${data.aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
+  token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+  #load_config_file       = false
+}
+  
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"

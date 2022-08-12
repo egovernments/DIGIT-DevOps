04a6f31472981df99ba8b24faccd645cc8a91b52
Subhashini Srinivasan <subha@Subhashinis-MacBook-Air.local>
2022-08-12 09:48:15 +0530

    Added comments to the Terraform variables.tf file for better understanding

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -2,20 +2,26 @@
 # Variables Configuration
 #
 
+# Name of the Kubernetes cluster
 variable "cluster_name" {
-  default = "my-first-eks"
+  default = "my-first-eks" #REPLACE
 }
 
 variable "vpc_cidr_block" {
   default = "192.168.0.0/16"
 }
 
+#Configure availability zones configuration for VPC. Leave as default for India.
+#Recommendation is to have subnets in at least two availability zones.
 variable "network_availability_zones" {
-  default = ["ap-south-1b", "ap-south-1a"]
+  default = ["ap-south-1b", "ap-south-1a"] #REPLACE IF NEEDED
 }
 
+#Amazon EKS runs and scales the Kubernetes control plane across multiple AWS Availability Zones to 
+#ensure high availability. Specify a comma separated list to have a cluster spanning multiple zones. Note
+#that this will have cost implications
 variable "availability_zones" {
-  default = ["ap-south-1b"]
+  default = ["ap-south-1b"] #REPLACE IF NEEDED
 }
 
 variable "kubernetes_version" {
@@ -26,11 +32,13 @@ variable "instance_type" {
   default = "m4.xlarge"
 }
 
+#eGov recommended defaults.
 variable "override_instance_types" {
   default = ["r5a.large", "r5ad.large", "r5d.large", "m4.xlarge"]
   
 }
 
+#eGov recommended default. 
 variable "number_of_worker_nodes" {
   default = "5"
 }
@@ -40,16 +48,20 @@ variable "ssh_key_name" {
 }
 
 variable "bucket_name" {
-  default = "try-workshop"
+  default = "try-workshop" #REPLACE
 }
 
+# RDS DB name. Make sure there are no hyphens or other special
+#characters in the DB name. Else, DB creation will fail.
 variable "db_name" {
-default = "digit_test"
+default = "digittest" #REPLACE
 }
 
+#RDS database user name
 variable "db_username" {
-default = "egovdemo"
+default = "egovdemo" #REPLACE
 }
 
+#DO NOT fill in here. This will be asked at runtime
 variable "db_password" {}
 

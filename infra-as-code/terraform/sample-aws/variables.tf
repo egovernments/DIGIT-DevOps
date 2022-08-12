fec8fd5ecacbab65c59710ff852c872e7a74cb98
Nikesh <nikeshumredkar@nikesh.local>
2022-08-12 11:32:40 +0530

    Added descriptions to aws tf's variable.tf

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -3,26 +3,32 @@
 #
 
 variable "cluster_name" {
-  default = "my-first-eks"
+  description = "Name of the Kubernetes cluster"
+  default = "my-first-eks" #REPLACE
 }
 
 variable "vpc_cidr_block" {
+  description = "CIDR block"
   default = "192.168.0.0/16"
 }
 
 variable "network_availability_zones" {
-  default = ["ap-south-1b", "ap-south-1a"]
+  description = "Configure availability zones configuration for VPC. Leave as default for India. Recommendation is to have subnets in at least two availability zones"
+  default = ["ap-south-1b", "ap-south-1a"] #REPLACE IF NEEDED
 }
 
 variable "availability_zones" {
-  default = ["ap-south-1b"]
+  description = "Amazon EKS runs and scales the Kubernetes control plane across multiple AWS Availability Zones to ensure high availability. Specify a comma separated list to have a cluster spanning multiple zones. Note that this will have cost implications"
+  default = ["ap-south-1b"] #REPLACE IF NEEDED
 }
 
 variable "kubernetes_version" {
+  description = "kubernetes version"
   default = "1.20"
 }
 
 variable "instance_type" {
+  description = "eGov recommended below instance type as a defaults"
   default = "m4.xlarge"
 }
 
@@ -32,24 +38,24 @@ variable "override_instance_types" {
 }
 
 variable "number_of_worker_nodes" {
-  default = "5"
+  description = "Worker node counts"
+  default = "5" #REPLACE IF NEEDED
 }
 
 variable "ssh_key_name" {
-  default = "my-first-eks"
-}
-
-variable "bucket_name" {
-  default = "try-workshop"
+  default = "my-first-eks" #REPLACE
 }
 
 variable "db_name" {
-default = "digit_test"
+  description = "RDS DB name. Make sure there are no hyphens or other special characters in the DB name. Else, DB creation will fail"
+  default = "digit_test" #REPLACE
 }
 
 variable "db_username" {
-default = "egovdemo"
+  description = "RDS database user name"
+  default = "egovdemo" #REPLACE
 }
 
+#DO NOT fill in here. This will be asked at runtime
 variable "db_password" {}
 

9ea77214adfde9853dda0022db7d691400765661
Rushang Dhanesha <rushangdhanesha@gmail.com>
2022-09-13 16:46:46 +0530

    Works QA Setup terraform files

---
+++ infra-as-code/terraform/works-qa/variables.tf
@@ -0,0 +1,44 @@
+#
+# Variables Configuration
+#
+
+variable "cluster_name" {
+  default = "works-qa"
+}
+
+variable "vpc_cidr_block" {
+  default = "192.168.0.0/16"
+}
+
+variable "network_availability_zones" {
+  default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "availability_zones" {
+  default = ["ap-south-1a"]
+}
+
+variable "kubernetes_version" {
+  default = "1.20"
+}
+
+variable "instance_type" {
+  default = "m4.xlarge"
+}
+
+variable "override_instance_types" {
+  default = ["r5a.large", "r5ad.large", "r5d.large", "m4.xlarge"]
+  
+}
+
+variable "number_of_worker_nodes" {
+  default = "4"
+}
+
+variable "ssh_key_name" {
+  default = "my-first-eks"
+}
+
+variable "bucket_name" {
+  default = "works-terraform-state-qa"
+}

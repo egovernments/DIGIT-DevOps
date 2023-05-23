ae72139a9a6fd938b9d73d6ee71e3664695facab
aniket <aniket@anikets-MacBook-Air.local>
2023-05-24 00:02:37 +0530

    added dependencies installation script for ubuntu and mac

--- infra-as-code/terraform/scripts/install_dependencies_ubuntu.sh
+++ infra-as-code/terraform/scripts/install_dependencies_ubuntu.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
 # Update and upgrade system packages
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
 # Install curl
-sudo apt install curl
+sudo apt install -y curl
 
 # Verify curl installation
 curl --version
 
 # Download and install kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.17/2023-03-17/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl
-export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --short --client
 
 # Install k9s
-snap install k9s
+sudo apt install -y k9s
 k9s version
 
 # Download and install aws-iam-authenticator
-curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_linux_amd64
+curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest/download/aws-iam-authenticator_`uname -s`_`uname -m`
 chmod +x ./aws-iam-authenticator
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator
-export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 aws-iam-authenticator help
 
 # Download and install AWS CLI
@@ -36,31 +32,28 @@ sudo ./aws/install
 aws --version
 
 # Install Terraform
-sudo snap install terraform --classic
+sudo apt install -y terraform
 terraform version
 
 # Update system packages
 sudo apt update
 
 # Install Git
-sudo apt install git
+sudo apt install -y git
 git --version
 
 # Install Go
-sudo snap install go
+sudo apt install -y golang-go
 
 # Add Helm GPG key
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 
 # Install apt-transport-https
-sudo apt-get install apt-transport-https --yes
+sudo apt-get install -y apt-transport-https
 
 # Add Helm repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
 # Update and install Helm
 sudo apt-get update
-sudo apt-get install helm
-
-# Make script executable
-chmod +x "$0"
+sudo apt-get install -y helm

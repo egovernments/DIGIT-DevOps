bdb35e3637f0d2b8cc92aa4e0a66b17db89b4b84
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2023-05-31 09:59:39 +0530

    Update main.tf
    Added kms key configuration in main.tf

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -23,6 +23,95 @@ module "db" {
   environment                   = "${var.cluster_name}"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "kms_key" {
+  description = "sops KMS key to encrypt and decrypt sops"
+  lifecycle {
+    ignore_changes = [tags]
+}
+
+  policy = <<EOF
+{
+    "Id": "key-consolepolicy-3",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${data.aws_caller_identity.current.arn}"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow access for Key Administrators",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${data.aws_caller_identity.current.arn}"
+            },
+            "Action": [
+                "kms:Create*",
+                "kms:Describe*",
+                "kms:Enable*",
+                "kms:List*",
+                "kms:Put*",
+                "kms:Update*",
+                "kms:Revoke*",
+                "kms:Disable*",
+                "kms:Get*",
+                "kms:Delete*",
+                "kms:TagResource",
+                "kms:UntagResource",
+                "kms:ScheduleKeyDeletion",
+                "kms:CancelKeyDeletion"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${data.aws_caller_identity.current.arn}"
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow attachment of persistent resources",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "${data.aws_caller_identity.current.arn}"
+            },
+            "Action": [
+                "kms:CreateGrant",
+                "kms:ListGrants",
+                "kms:RevokeGrant"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Bool": {
+                    "kms:GrantIsForAWSResource": "true"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_kms_alias" "kms_alias" {
+  name          = "alias/padma-key"
+  target_key_id = aws_kms_key.kms_key.key_id
+}
+
 data "aws_eks_cluster" "cluster" {
   name = "${module.eks.cluster_id}"
 }

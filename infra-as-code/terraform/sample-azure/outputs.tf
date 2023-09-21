ca402856e06b46841d8be74ee42248c1f158188b
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2023-09-21 14:29:46 +0530

    Update outputs.tf

--- infra-as-code/terraform/sample-azure/outputs.tf
+++ infra-as-code/terraform/sample-azure/outputs.tf
@@ -1,19 +1,3 @@
-output "zookeeper_storage_ids" {
-  value = "${module.zookeeper.storage_ids}"
-}
-
-output "kafka_storage_ids" {
-  value = "${module.kafka.storage_ids}"
-}
-
-output "es_master_storage_ids" {
-  value = "${module.es-master.storage_ids}"
-}
-
-output "es_data_v1_storage_ids" {
-  value = "${module.es-data-v1.storage_ids}"
-}
-
 output "azurerm_postgresql_flexible_server" {
   value = module.postgres-db.azurerm_postgresql_flexible_server
 }

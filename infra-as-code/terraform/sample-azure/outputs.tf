ba5867766d27814742559a16fbc180630b01f54b
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2023-09-21 21:28:40 +0530

    Update outputs.tf

--- infra-as-code/terraform/sample-azure/outputs.tf
+++ infra-as-code/terraform/sample-azure/outputs.tf
@@ -5,3 +5,15 @@ output "azurerm_postgresql_flexible_server" {
 output "postgresql_flexible_server_database_name" {
   value = module.postgres-db.postgresql_flexible_server_database_name
 }
+
+output "resource_group"{
+  value = var.resource_group
+}
+
+output "db_user" {
+  value = var.db_user
+}
+
+output "cluster_name" {
+  value = var.environment
+}

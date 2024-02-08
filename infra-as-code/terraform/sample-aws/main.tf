89e4ac5f314a61403c3db6048d7dac7fe917f044
Harish S <119594385+Harish-egov@users.noreply.github.com>
2024-02-08 11:45:57 +0530

    updated release charts and terraform script (#2404)
    Co-authored-by: Harish-egov <harish,s@egovernmetns.org>

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -43,6 +43,12 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "${module.eks.cluster_id}"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "tls_certificate" "thumb" {
+  url = "${data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer}"
+}
+
 provider "kubernetes" {
   host                   = "${data.aws_eks_cluster.cluster.endpoint}"
   cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
@@ -62,6 +68,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "spot"
+      ami_id                        = "ami-0a82b544ef71a207d"   
       subnets                       = "${concat(slice(module.network.private_subnets, 0, length(var.availability_zones)))}"
       instance_type                 = "${var.instance_type}"
       override_instance_types       = "${var.override_instance_types}"
@@ -78,7 +85,57 @@ module "eks" {
       "KubernetesCluster" = "${var.cluster_name}"
     })
   }"
- 
+}
+
+resource "aws_iam_role" "eks_iam" {
+  name = "${var.cluster_name}-eks"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "EKSWorkerAssumeRole"
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "kubernetes_annotations" "example" {
+  api_version = "v1"
+  kind        = "ServiceAccount"
+  metadata {
+    name = "ebs-csi-controller-sa"
+    namespace = "kube-system"
+  }
+  annotations = {
+    "eks.amazonaws.com/role-arn" = "${aws_iam_role.eks_iam.arn}"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEBSCSIDriverPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = "${aws_iam_role.eks_iam.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEC2FullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = "${aws_iam_role.eks_iam.name}"
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["${data.tls_certificate.thumb.certificates.0.sha1_fingerprint}"] # This should be empty or provide certificate thumbprints if needed
+  url            = "${data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer}" # Replace with the OIDC URL from your EKS cluster details
 }
 
 resource "aws_security_group_rule" "rds_db_ingress_workers" {
@@ -89,6 +146,23 @@ resource "aws_security_group_rule" "rds_db_ingress_workers" {
   security_group_id        = "${module.network.rds_db_sg_id}"
   source_security_group_id = "${module.eks.worker_security_group_id}"
   type                     = "ingress"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = data.aws_eks_cluster.cluster.name
+  addon_name        = "kube-proxy"
+  resolve_conflicts = "OVERWRITE"
+}
+resource "aws_eks_addon" "core_dns" {
+  cluster_name      = data.aws_eks_cluster.cluster.name
+  addon_name        = "coredns"
+  resolve_conflicts = "OVERWRITE"
+}
+resource "aws_eks_addon" "aws_ebs_csi_driver" {
+  cluster_name      = data.aws_eks_cluster.cluster.name
+  addon_name        = "aws-ebs-csi-driver"
+  addon_version     = "v1.23.0-eksbuild.1"
+  resolve_conflicts = "OVERWRITE"
 }
 
 module "es-master" {

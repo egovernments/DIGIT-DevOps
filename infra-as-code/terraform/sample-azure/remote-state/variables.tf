b5154a2d1e45867f05371f2be9b88557f3e2082f
nikhilmulinti <nikhil.mulinti@egovernments.org>
2024-09-25 11:25:39 +0530

    pushing latest changes

--- infra-as-code/terraform/sample-azure/remote-state/variables.tf
+++ infra-as-code/terraform/sample-azure/remote-state/variables.tf
@@ -1,23 +1,4 @@
-# Define Azure authentication variables
-variable "subscription_id" {
-  description = "The subscription ID to use for Azure"
-  default = "d5fae6d0-9ecb-40a9-b66b-7583559ce79e"
-}
 
-variable "tenant_id" {
-  description = "The tenant ID to use for Azure"
-  default = "593ce202-d1a9-4760-ba26-ae35417c00cb"
-}
-
-variable "client_id" {
-  description = "The client ID for the Azure service principal"
-  default = "7cc44878-cfd6-4eb9-ab24-e5e855786224"
-}
-
-variable "client_secret" {
-  description = "The client secret for the Azure service principal"
-  default = "jb38Q~0.F2yg1TWdUA7lE4yjQggCcJSocJWtCcgu"
-}
 
 # Other variables
 variable "tfstate" {

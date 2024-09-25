b2d055402ed2397281d05721d167416c8a7d6e56
nikhilmulinti <127198713+nikhilmulinti@users.noreply.github.com>
2024-09-25 12:38:57 +0530

    Update variables.tf

--- infra-as-code/terraform/sample-azure/variables.tf
+++ infra-as-code/terraform/sample-azure/variables.tf
@@ -18,5 +18,23 @@ variable "db_user" {
 }
 
 variable "db_password"{}
+variable "subscription_id" {
+  description = "The Subscription ID for Azure"
+  type        = string
+}
 
+variable "tenant_id" {
+  description = "The Tenant ID for Azure Active Directory"
+  type        = string
+}
 
+variable "client_id" {
+  description = "The Client ID for Azure Active Directory Application"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret for Azure Active Directory Application"
+  type        = string
+  sensitive   = true
+}

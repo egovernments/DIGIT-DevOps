2c89d236a1bf1f4d86dad5492de25440c533de20
SriPadma8997-egov <sri.padma@egovernments.org>
2025-06-25 17:23:17 +0530

    updated infra for karpenter

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -31,7 +31,7 @@ variable "kubernetes_version" {
 
 variable "instance_types" {
   description = "eGov recommended below instance type as a default"
-  default = ["r6g.xlarge"]
+  default = ["t4g.xlarge"]
 }
 
 variable "min_worker_nodes" {
@@ -41,7 +41,7 @@ variable "min_worker_nodes" {
 
 variable "desired_worker_nodes" {
   description = "eGov recommended below worker node counts as default for desired nodes"
-  default = "4" #REPLACE IF NEEDED
+  default = "1" #REPLACE IF NEEDED
 }
 
 variable "max_worker_nodes" {

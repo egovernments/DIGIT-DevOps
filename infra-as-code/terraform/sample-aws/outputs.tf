a8e47fb0d5375ff3ac4e52aa6a710193b1cf9d0b
shivam-eGov <shivam.upadhyay@egovernments.org>
2025-07-17 10:44:29 +0530

    strip lines

--- infra-as-code/terraform/sample-aws/outputs.tf
+++ infra-as-code/terraform/sample-aws/outputs.tf
@@ -10,7 +10,6 @@ output "public_subnets" {
   value = module.network.public_subnets
 }
 
-
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane."
   value       = module.eks.cluster_endpoint
@@ -19,7 +18,6 @@ output "cluster_endpoint" {
 output "db_instance_endpoint" {
   value = module.db.db_instance_endpoint
 }
-
 
 output "db_instance_name" {
   description = "The database name"

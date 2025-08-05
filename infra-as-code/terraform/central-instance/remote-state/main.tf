9bf51367e0fbb5ff32c1ffa9e484621c54194d4a
shivam-eGov <shivam.upadhyay@egovernments.org>
2025-08-05 10:54:22 +0530

    updated terraform scripts

--- infra-as-code/terraform/central-instance/remote-state/main.tf
+++ infra-as-code/terraform/central-instance/remote-state/main.tf
@@ -3,19 +3,23 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "central-instance-test-terraform-state"
-
-  versioning {
-    enabled = true
-  }
+  bucket = "${var.bucket_name}"
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "central-instance-test-terraform-state"
+  name           = "${var.bucket_name}"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
---
+++ infra-as-code/terraform/central-instance/variables.tf
@@ -0,0 +1,82 @@
+#
+# Variables Configuration. Check for REPLACE to substitute custom values. Check the description of each
+# tag for more information
+#
+
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
+  default = "central-instance" #REPLACE
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block"
+  default = "192.168.0.0/16"
+}
+
+variable "network_availability_zones" {
+  description = "Configure availability zones configuration for VPC. Leave as default for India. Recommendation is to have subnets in at least two availability zones"
+  default = ["ap-south-1a", "ap-south-1b"] #REPLACE IF NEEDED
+}
+
+variable "availability_zones" {
+  description = "Amazon EKS runs and scales the Kubernetes control plane across multiple AWS Availability Zones to ensure high availability. Specify a comma separated list to have a cluster spanning multiple zones. Note that this will have cost implications"
+  default = ["ap-south-1a"] #REPLACE IF NEEDED
+}
+
+variable "kubernetes_version" {
+  description = "kubernetes version"
+  default = "1.31"
+}
+
+variable "instance_types" {
+  description = "Arry of instance types for SPOT instances"
+  default = ["t3a.xlarge"]
+}
+
+variable "min_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for min nodes"
+  default = "1" #REPLACE IF NEEDED
+}
+
+variable "desired_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for desired nodes"
+  default = "5" #REPLACE IF NEEDED
+}
+
+variable "max_worker_nodes" {
+  description = "eGov recommended below worker node counts as default for max nodes"
+  default = "9" #REPLACE IF NEEDED
+}
+
+variable "db_name" {
+  description = "RDS DB name. Make sure there are no hyphens or other special characters in the DB name. Else, DB creation will fail"
+  default = "centralinstancedb" #REPLACE
+}
+
+variable "db_username" {
+  description = "RDS database user name"
+  default = "centralinstance" #REPLACE
+}
+
+variable "iam_user_arn" {
+  description = "Provide the IAM user arn which you are using to create infrastructure"
+  default = "arn:aws:iam::349271159511:user/central-instance-admin" #Example: arn:aws:iam::4332145635273:user/demo
+}
+
+variable "ami_id" {
+  description = "Provide the AMI ID that supports your eks version"
+  default = {
+    id   = "ami-0d1008f82aca87cb9"
+    name = "amazon-eks-node-1.30-v20241024"
+  }
+}
+
+variable "enable_karpenter" {
+  description = "Enable the karpenter."
+  type        = bool
+  default     = false
+}
+
+#DO NOT fill in here. This will be asked at runtime
+variable "db_password" {}
+

90e1df02b6f7b74fa7af5dad2bb93bb0ff51a52d
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2026-04-06 18:55:24 +0530

    Removed unnecessary modules and added 1.33 code in the master branch (#4434)

--- infra-as-code/terraform-upgrade/modules/storage/azure/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "environment" {}
-variable "itemCount" {}
-variable "disk_prefix" {}    
-variable "location" {}
-variable "resource_group" {}
-variable "storage_sku" {}
-variable "disk_size_gb" {}
--- infra-as-code/terraform/node-pool/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "node_group" {
-  description = "Outputs of node group"
-  value       = aws_eks_node_group.main
-}
